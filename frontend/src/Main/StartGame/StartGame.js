4c73971df80d61e71ab26232abe5bfeeb9828730
Anshul Goyal <anshulgoel151999@gmail.com>
2020-03-30 22:34:08 +0530

    Fixed rules

--- frontend/src/Main/StartGame/StartGame.js
+++ frontend/src/Main/StartGame/StartGame.js
@@ -1,56 +1,53 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import Uppericons from '../MainLayout/uppericons'
-import Lowericons from '../MainLayout/lowericons'
-import MobileUpper from '../MainLayout/MobileView/UpperMobile'
-import LowerMobile from '../MainLayout/MobileView/LowerMobile'
-import {Link} from 'react-router-dom'
+import React from "react";
+import ReactDom from "react-dom";
+import Uppericons from "../MainLayout/uppericons";
+import Lowericons from "../MainLayout/lowericons";
+import MobileUpper from "../MainLayout/MobileView/UpperMobile";
+import LowerMobile from "../MainLayout/MobileView/LowerMobile";
+import { Link } from "react-router-dom";
 const StartGame = () => {
-    return (
-
-        <div className = "StartGame" className="mobilecolumn">>
-        <br />
-        <div>
-           < MobileUpper />
-        </div>
-        <div class = "row" >
-        <div class = "col-md-2" >
-        <Uppericons / >
+  return (
+    <div className="StartGame" className="mobilecolumn">
+      >
+      <br />
+      <div>
+        <MobileUpper />
+      </div>
+      <div class="row">
+        <div class="col-md-2">
+          <Uppericons />
         </div>
         <div class="col-md-8">
-        
-        <div  className="RulesContainer">
-        
-        
-        <ol className="RulesListStartGame" >
-       <li>Everytime, someone before you gets the right a answer, the maximum point of that question lowers. </li>
+          <div className="RulesContainer">
+            <ol className="RulesListStartGame">
+              <li>
+                Everytime, someone before you gets a right answer, the maximum
+                score for that question lowers.{" "}
+              </li>
               <li>Usage of hints will cost you some points.</li>
               <li>A question can have multiple answers.</li>
               <li>Minimum marks for a question are 20</li>
               <li>Maximum marks for a question are 100</li>
               <li>Their is no negative marking</li>
-        </ol>
+            </ol>
 
-        <Link to='./question'>
-        <img src={require('../icons/START THE GAME.png')} className="StartTheGame"  />
-        </Link>  
-        
+            <Link to="./question">
+              <img
+                src={require("../icons/START THE GAME.png")}
+                className="StartTheGame"
+              />
+            </Link>
+          </div>
         </div>
-         
+        <div class="col-md-2">
+          <Lowericons />
         </div>
-        < div class = "col-md-2" >
-        <Lowericons />
+        <div class="col-md-2">
+          <LowerMobile />
         </div>
-        < div class = "col-md-2" >
-        <LowerMobile />
-        </div>
-        
-
-        </div>
-</div>
-        
-        
-            );
-}
+      </div>
+    </div>
+  );
+};
 
 export default StartGame;
