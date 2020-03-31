4a617602af7bafe0a537fdd4407c957abad28b8b
Sanyam Mittal <sanyam19092000@gmail.com>
2020-03-31 14:29:42 +0530

    only <16

--- backend/core/models.py
+++ backend/core/models.py
@@ -129,7 +129,7 @@ class Submission(models.Model):
 
             #### Marking Scheme ####
 
-            if CheckHappyHour(start, now, end) and self.ques.no<17:
+            if CheckHappyHour(start, now, end) and self.ques.no<16:
                 score = 80
             else:
                 if self.hintviewed:
