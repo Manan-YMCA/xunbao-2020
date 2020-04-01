1553aecd2c859d76ad8762e41601c0b509a42cb1
Sanyam Mittal <sanyam19092000@gmail.com>
2020-04-01 15:00:46 +0530

    happy hour till 23rd

--- backend/core/models.py
+++ backend/core/models.py
@@ -129,7 +129,7 @@ class Submission(models.Model):
 
             #### Marking Scheme ####
 
-            if CheckHappyHour(start, now, end) and self.ques.no<16:
+            if CheckHappyHour(start, now, end) and self.ques.no < 24:
                 score = 80
             else:
                 if self.hintviewed:
